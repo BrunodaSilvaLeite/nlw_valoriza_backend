b3e641093f37c6dcf4254ada8b29d06d737f18e2
Bruno <bruno.etiel.silva@gmail.com>
2021-08-31 17:44:43 -0300

    feat(ListUser): Add ListUser

---
+++ src/controllers/ListUserController.ts
@@ -0,0 +1,15 @@
+import {Request, Response } from "express";
+import { ListUserService } from "../service/ListUserService";
+
+class ListUserController {
+    async handle(request: Request, response: Response){
+
+        const listUserService = new ListUserService();
+
+        const users = await listUserService.execute();
+
+        return response.json(users)
+    }
+}
+
+export { ListUserController}
